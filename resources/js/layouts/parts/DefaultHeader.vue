c5c3bbde48aca421b09a400d754bf07849cfd481
compico <compico@mail.ru>
2025-07-27 15:03:50 +0300

    фикс тестов

--- resources/js/layouts/parts/DefaultHeader.vue
+++ resources/js/layouts/parts/DefaultHeader.vue
@@ -1,26 +1,7 @@
 <script setup lang="ts">
-import {
-    NavigationMenuContent,
-    NavigationMenuItem,
-    NavigationMenuList,
-    NavigationMenuRoot,
-    NavigationMenuTrigger,
-    NavigationMenuViewport,
-} from 'reka-ui'
+
 </script>
 
 <template>
-    <NavigationMenuRoot>
-        <NavigationMenuList>
-            <NavigationMenuItem>
-                <NavigationMenuTrigger>Item one</NavigationMenuTrigger>
-                <NavigationMenuContent>Item one content</NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-                <NavigationMenuTrigger>Item two</NavigationMenuTrigger>
-                <NavigationMenuContent>Item two content</NavigationMenuContent>
-            </NavigationMenuItem>
-        </NavigationMenuList>
-        <NavigationMenuViewport />
-    </NavigationMenuRoot>
+
 </template>
